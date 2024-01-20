3a13f61bd5e961b79bfff4c0c6de37fe4ebeda65
исп-32 <-32@LAB30-01>
2024-01-20 12:40:26 +0300

    Made code refactoring

--- StudentsApp/Windows/MainWindow.xaml.cs
+++ StudentsApp/Windows/MainWindow.xaml.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
 using StudentsApp.Models;
 using StudentsApp.Windows;
-using System.Collections.Generic;
 
 namespace StudentsApp
 {
@@ -16,32 +16,68 @@ namespace StudentsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Поле фильтрации
+        const int FILTER_BY_ADMISSION_YEAR = 1;     // Фильтр по году поступления
+        const int FILTER_BY_BIRTH_YEAR     = 2;     // Фильтр по году рождения
+
+        // Поле сортировки
+        const int SORT_BY_STUDENT_ID   = 0,     // Сортировка по № студенческого билета
+                  SORT_BY_LAST_NAME    = 1,     // Сортировка по фамилии
+                  SORT_BY_FIRST_NAME   = 2,     // Соортировка по имени
+                  SORT_BY_MIDDLE_NAME  = 3,     // Сортировка по отчеству
+                  SORT_BY_BIRTH_DATE   = 4,     // Сортировка по дате рождения
+                  SORT_BY_ADDRESS      = 5,     // Сортировка по адресу
+                  SORT_BY_PHONE_NUMBER = 6;     // Сортировка по номеру телефона
+
+        // Тип сортировки
+        const int ASC_SORT  = 1,    // Сортировка по возрастанию
+                  DESC_SORT = 2;    // Сортировка по убыванию
+
         StudentsContext _db = new StudentsContext();
+        List<Student> _students = new List<Student>();
+
+        // Распределение по страницам
+        int _currentPage = 1,
+            _maxPage = 0;
 
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        /// <summary>
-        /// Загрузка данных в DataGrid
-        /// </summary>
-        private void LoadDbInDataGrid()
-        {
             _db.Students.Load();
-            dgStudents.ItemsSource = _db.Students.ToList();
+            _students = _db.Students.ToList();
+
+            cmbBoxFilterType.SelectedIndex = 0;
+            cmbBoxFilterType.SelectedIndex = 0;
+
+            cmbBoxSortField.SelectedIndex = 0;
+            cmbBoxSortType.SelectedIndex = 0;
+
+            cmbBoxStudentCount.SelectedIndex = 0;
+
+            UpdateStudentList();
         }
 
+        #region CRUD operations
         /// <summary>
-        /// Обновление данных в DataGrid
+        /// Обновление списка студентов на основе сортировки, поиска, фильтрации и деления на страницы
         /// </summary>
-        /// <param name="grid">DataGrid для обновления</param>
-        private void UpdateDataGrid(DataGrid grid)
+        private void UpdateStudentList()
         {
-            grid.ItemsSource = null;
-            grid.ItemsSource = _db.Students.
-                               OrderBy(student => student.StudentId).
-                               ToList();
+            string request = txtBoxSearch.Text.
+                                          Replace(" ", "").
+                                          ToLower();
+
+            // Список формировать в порядке
+            // сортировка -> поиск -> фильтрация -> деление на страницы
+            List<Student> studentList = SearchStudents(
+                                        GetPages(_students), request);
+
+            txtBoxCurrentPage.Text = _currentPage.ToString();
+            txtBoxCurrentPage.MaxLength = _maxPage.ToString().Length;
+            txtBoxTotalPage.Text = _maxPage.ToString();
+
+            dgStudents.ItemsSource = studentList;
         }
 
         /// <summary>
@@ -53,39 +89,100 @@ namespace StudentsApp
             AddEditStudentWindow addEditStudentWindow = new AddEditStudentWindow(student);
             addEditStudentWindow.ShowDialog();
 
-            UpdateDataGrid(dgStudents);
+            UpdateStudentList();
         }
 
         /// <summary>
-        /// Загрузка окна
+        /// Кнопка "Добавить"
         /// </summary>
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            LoadDbInDataGrid();
-            UpdateDataGrid(dgStudents);
+            ShowAddOrEditWindows(null);
         }
 
+        /// <summary>
+        /// Кнопка "Редактировать"
+        /// </summary>
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Student? editedStudent = (sender as Button).DataContext as Student;
+
+            ShowAddOrEditWindows(editedStudent);
+        }
+
+        /// <summary>
+        /// Кнопка "Удалить"
+        /// </summary>
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result =
+                MessageBox.Show(
+                    "Удалить запись?",
+                    "Удаление записи",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning
+                    );
+
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Student? deletedStudent = (sender as Button).DataContext as Student;
+
+                    _db.Students.Remove(deletedStudent);
+
+                    MessageBox.Show(
+                        "Запись удалена",
+                        "Информация",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Information
+                        );
+
+                    _db.SaveChanges();
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+
+                    MessageBox.Show(
+                        "Выберите запись для удаления",
+                        "Предупреждение",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+                }
+            }
+
+            UpdateStudentList();
+        }
+        #endregion
+
+        #region Search
         /// <summary>
         /// Поиск студента по ФИО
         /// </summary>
-        private void txtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        /// <param name="students">Список студентов для поиска</param>
+        /// <param name="request">Поисковый запрос</param>
+        /// <returns>Результаты поиска</returns>
+        private List<Student> SearchStudents(List<Student> students, string request)
         {
-            List<Student> currentStudent = new List<Student>();
-
-            string request = txtBoxSearch.Text.
-                                          Replace(" ", "").
-                                          ToLower();
-
-            currentStudent = _db.Students.Where(s => (s.LastName + 
-                                                      s.FirstName +
-                                                      s.MiddleName).
-                                                           ToLower().
-                                                           Contains(request)).
-                                                           ToList();
-
-            dgStudents.ItemsSource = currentStudent;
+            return students.Where(s => (s.LastName +
+                                        s.FirstName +
+                                        s.MiddleName).
+                                             ToLower().
+                                             Contains(request)).
+                                             ToList();
         }
 
+        /// <summary>
+        /// Изменение поискового запроса
+        /// </summary>
+        private void txtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateStudentList();
+        }
+        #endregion
+
+        #region Filter
         /// <summary>
         /// <para>Получение списка годов поступления студентов</para>
         /// <para>Загрузка списка в ComboBox</para>
@@ -143,10 +240,9 @@ namespace StudentsApp
             filterType.SelectedIndex = 0;
         }
 
-        // Фильтр по году рождения и по году поступления
-        const int FILTER_BY_ADMISSION_YEAR = 1;
-        const int FILTER_BY_BIRTH_YEAR = 2;
-
+        /// <summary>
+        /// Выбор поля для фильтрации
+        /// </summary>
         private void cmbBoxFilterField_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             switch (cmbBoxFilterField.SelectedIndex)
@@ -167,123 +263,123 @@ namespace StudentsApp
             }
         }
 
+        /// <summary>
+        /// Выбор типа значения фильтрации
+        /// </summary>
         private void cmbBoxFilterType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+        #endregion
 
+        #region Sort
+        /// <summary>
+        /// Выбор поля для сортировки
+        /// </summary>
         private void cmbBoxSortField_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Выбор типа сортировки
+        /// </summary>
         private void cmbBoxSortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+        #endregion
+
+        #region Pages
+        /// <summary>
+        /// Деление списка студентов на страницы
+        /// </summary>
+        /// <param name="students">Список студентов</param>
+        /// <returns>Страница из списка студентов</returns>
+        private List<Student> GetPages(List<Student> students)
+        {
+            int pageSize = (cmbBoxStudentCount.SelectedIndex + 1) * 5;
+
+            _maxPage = (int)Math.Ceiling(
+                       (double)_db.Students.Count() / pageSize);
+
+            return students.Skip((_currentPage - 1) * pageSize).
+                            Take(pageSize).
+                            ToList();
+        }
 
         /// <summary>
-        /// Кнопка "Добавить"
+        /// Выбор количества студентов на странице
         /// </summary>
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        private void cmbBoxStudentCount_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ShowAddOrEditWindows(null);
+            UpdateStudentList();
         }
 
         /// <summary>
-        /// Кнопка "Редактировать"
+        /// Переход на первую страницу
         /// </summary>
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
-        {
-            Student? editedStudent = (sender as Button).DataContext as Student;
-
-            ShowAddOrEditWindows(editedStudent);
-        }
-
-        /// <summary>
-        /// Кнопка "Удалить"
-        /// </summary>
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBoxResult result =
-                MessageBox.Show(
-                    "Удалить запись?",
-                    "Удаление записи",
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Warning
-                    );
-
-            if ( result == MessageBoxResult.Yes )
-            {
-                try
-                {
-                    Student? deletedStudent = (sender as Button).DataContext as Student;
-
-                    _db.Students.Remove(deletedStudent);
-
-                    MessageBox.Show(
-                        "Запись удалена",
-                        "Информация",
-                        MessageBoxButton.OK,
-                        MessageBoxImage.Information
-                        );
-
-                    _db.SaveChanges();
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-
-                    MessageBox.Show(
-                        "Выберите запись для удаления",
-                        "Предупреждение",
-                        MessageBoxButton.OK,
-                        MessageBoxImage.Warning
-                        );
-                }
-            }
-
-            UpdateDataGrid(dgStudents);
-        }
-
-        private void cBoxPageCount_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            int currentPage = 1;
-
-            int pageSize = (cBoxPageCount.SelectedIndex + 1) * 5;
-
-            int maxPage = (int)Math.Ceiling((double)_db.Students.Count()  / pageSize);
-
-            var studentsList = _db.Students.ToList().Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
-
-            txtBoxCurrentPage.Text = currentPage.ToString();
-            txtBoxTotalPage.Text = maxPage.ToString();
-
-            dgStudents.ItemsSource = studentsList;
-        }
-
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
+            _currentPage = 1;
 
+            UpdateStudentList();
         }
 
-        private void btnPrev_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void txtBoxCurrentPage_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void btnNext_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Переход на последнюю страницу
+        /// </summary>
         private void btnEnd_Click(object sender, RoutedEventArgs e)
         {
+            _currentPage = _maxPage;
 
+            UpdateStudentList();
         }
+
+        /// <summary>
+        /// Переход на предыдущую страницу
+        /// </summary>
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentPage - 1 < 1)
+            {
+                return;
+            }
+
+            _currentPage -= 1;
+
+            UpdateStudentList();
+        }
+
+        /// <summary>
+        /// Переход на следующую страницу
+        /// </summary>
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if ( _currentPage + 1 > _maxPage)
+            {
+                return;
+            }
+
+            _currentPage += 1;
+
+            UpdateStudentList();
+        }
+
+        /// <summary>
+        /// Изменение номера текущей страницы
+        /// </summary>
+        private void txtBoxCurrentPage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_currentPage >= 1 &&
+                _currentPage <= _maxPage &&
+                txtBoxCurrentPage.Text != string.Empty)
+            {
+                _currentPage = Convert.ToInt32(txtBoxCurrentPage.Text);
+            }
+
+            UpdateStudentList();
+        }
+        #endregion
     }
 }
