5c45de41ac2cbd713db91000644d40e32d3e7285
Александр Шустов <sasha.shustov.05@mail.ru>
2024-02-15 19:04:24 +0300

    d

--- StudentsApp/Models/StudentsContext.cs
+++ StudentsApp/Models/StudentsContext.cs
@@ -23,7 +23,7 @@ public partial class StudentsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=Syden1810; Database=Students; Integrated Security=true; Encrypt=false");
+        => optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress; Database=Students; User=ИСП-32; Password=1234567890; Encrypt=false");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
